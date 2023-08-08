a87a6372a199b14498fd61a4a35545f8063e865a
Abhay Vishwakarma <abhay.vishwakarma@neosoftmail.com>
2023-08-08 15:53:51 +0530

    Upload profile function relocated to commons service

--- src/main/java/com/blogstack/BlogStackUserManagementService.java
+++ src/main/java/com/blogstack/BlogStackUserManagementService.java
@@ -6,7 +6,6 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
-@SpringBootApplication
 @EnableCaching
 @EnableRedisRepositories
 @EnableFeignClients
--- src/main/java/com/blogstack/controllers/BlogStackUserController.java
+++ src/main/java/com/blogstack/controllers/BlogStackUserController.java
@@ -6,11 +6,9 @@ import com.blogstack.commons.BlogStackMessageConstants;
 import com.blogstack.service.IBlogStackAuthenticationService;
 import com.blogstack.service.IBlogStackS3BucketPhotoUploadService;
 import com.blogstack.service.IBlogStackUserService;
-import com.blogstack.service.impl.BlogStackAuthenticationService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,13 +17,8 @@ import java.io.IOException;
 
 @RestController
 @RequestMapping(path = "${iam-service-version}/user")
-
 // @CrossOrigin("*")
-
-
 public class BlogStackUserController {
-
-
     private IBlogStackUserService blogStackUserService;
     private IBlogStackS3BucketPhotoUploadService blogStackS3BucketProfilePhotoUploadService;
     private IBlogStackAuthenticationService blogStackAuthenticationService;
@@ -35,7 +28,6 @@ public class BlogStackUserController {
         this.blogStackUserService = blogStackUserService;
         this.blogStackS3BucketProfilePhotoUploadService = blogStackS3BucketProfilePhotoUploadService;
         this.blogStackAuthenticationService = blogStackAuthenticationService;
-
     }
 
     @GetMapping(value = "/{email_id}")
@@ -59,16 +51,17 @@ public class BlogStackUserController {
     }
 
     @PutMapping(value = "/profile-photo/{email_id}")
-    public ResponseEntity<?> updateProfilePhoto(@PathVariable(value = "email_id") @NotBlank(message = BlogStackMessageConstants.EMAIL_CANT_BLANK) String emailId, @RequestParam(value = "profile_pic")MultipartFile profilePic) throws IOException {
-        return this.blogStackS3BucketProfilePhotoUploadService.uploadProfilePhoto(emailId,profilePic);
+    public ResponseEntity<?> updateProfilePhoto(@PathVariable(value = "email_id") @NotBlank(message = BlogStackMessageConstants.EMAIL_CANT_BLANK) String emailId, @RequestParam(value = "profile_pic") MultipartFile profilePic) throws IOException {
+        return this.blogStackS3BucketProfilePhotoUploadService.uploadProfilePhoto(emailId, profilePic);
     }
 
     @PostMapping(value = "/forgot-password")
-    public ResponseEntity<?> forgotPassword(@RequestParam("email") String blogStackUserEmail, @RequestParam("uid") String blogStackUserId){
-        return this.blogStackAuthenticationService.forgotPasswordEmailGeneration(blogStackUserEmail,blogStackUserId);
+    public ResponseEntity<?> forgotPassword(@RequestParam("email") String blogStackUserEmail, @RequestParam("uid") String blogStackUserId) {
+        return this.blogStackAuthenticationService.forgotPasswordEmailGeneration(blogStackUserEmail, blogStackUserId);
     }
+
     @PostMapping(value = "/validate-otp")
-    public ResponseEntity<?> validateOtp(@RequestBody BlogStackForgotPasswordBean forgotPasswordBean){
+    public ResponseEntity<?> validateOtp(@RequestBody BlogStackForgotPasswordBean forgotPasswordBean) {
         return this.blogStackAuthenticationService.blogStackValidateOtp(forgotPasswordBean);
     }
 }
